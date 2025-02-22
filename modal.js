1503fb7a89d9aa06473b4aea907be615cf40e44a
AnastasiaPateli <70472292+AnastasiaPateli@users.noreply.github.com>
2025-02-22 18:14:48 +0200

    Create modal.js

---
+++ modal.js
@@ -0,0 +1,21 @@
+// modal.js
+
+// Function to open the modal
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+// Function to close the modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Close modal when clicking outside of the modal content
+window.onclick = function(event) {
+    var modals = document.getElementsByClassName("modal");
+    for (var i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
